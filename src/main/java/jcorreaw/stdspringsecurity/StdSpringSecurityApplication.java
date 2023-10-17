e86953e4c22a91cc62e8054a3265a7d64d847a88
jcorreaw <juninhocorrea2014@gmail.com>
2023-10-17 18:15:19 -0300

    projeto criado

---
+++ src/main/java/jcorreaw/stdspringsecurity/StdSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package jcorreaw.stdspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StdSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StdSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jcorreaw/stdspringsecurity/StdSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package jcorreaw.stdspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StdSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
